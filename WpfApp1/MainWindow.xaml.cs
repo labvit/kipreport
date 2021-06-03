36a1d59f2403a1699a35c8f4c73aea4caf6a28e9
labvit <labvit@gmail.com>
2021-06-03 18:29:07 +0300

    init

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    interface IDrawCanvas
+    {
+        void addLine(DrawLine l);
+        void addCircle(DrawCircle c);
+        void addRectangle(DrawBox b);
+    }
+    abstract class DrawShape
+    {
+        public abstract void DrawTo();
+    }
+
+    class DrawCircle : DrawShape { }
+    class DrawBox : DrawShape { }
+    class DrawLine : DrawShape
+    {
+        public override void DrawTo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class ShapeGroup { }
+    class Object2d { }
+
+    class Scene
+    {
+
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
